15adc3266fa2adb30400c14cbde6cd2f08e6924c
Dominic Marcuse <dominic@marcuse.us>
2019-12-18 23:03:18 -0500

    Update example to be cross-platform

--- examples/load.rs
+++ examples/load.rs
@@ -1,11 +1,12 @@
 extern crate dynamic_ocl;
 
+use dlopen::utils::platform_file_name;
 use dlopen::wrapper::Container;
 use dynamic_ocl::raw::OpenCL;
 
 pub fn main() {
-    let ocl: Container<OpenCL> =
-        unsafe { Container::load("libOpenCL.so") }.expect("Error loading OpenCL library");
+    let ocl: Container<OpenCL> = unsafe { Container::load(platform_file_name("OpenCL")) }
+        .expect("Error loading OpenCL library");
 
     println!("Successfully loaded OpenCL: {:#?}", *ocl);
 }
